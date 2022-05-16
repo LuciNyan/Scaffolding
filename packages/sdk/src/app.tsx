f31ab558fbc5ec2dbe8678e208e7c935d74dd771
luci <me@lucinyan.com>
2022-05-16 11:42:42 +0800

    chore: 3

--- packages/sdk/src/app.tsx
+++ packages/sdk/src/app.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { GlobalStyles } from './styles/GlobalStyles'
 
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const Text = styled.div`
+  width: 200px;
+  height: 100px;
+  border: solid red 1px;
+`
+
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <GlobalStyles />
-      <div>Hello World</div>
-    </React.Fragment>
+      <Main>
+        <Text>Hello World</Text>
+      </Main>
+    </>
   )
 }
 
